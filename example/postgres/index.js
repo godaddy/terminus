b173f788dd6f4ce08e09823cb923ddf6aa78e5c2
Łukasz Szewczak <lukaszewczak@gmail.com>
2018-01-12 22:28:54 +0100

    [feat] Add postgres example

---
+++ example/postgres/index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const http = require('http');
+const { Client } = require('pg');
+const terminus = require('../../lib/terminus');
+const app = express();
+const client = new Client({
+  database: 'terminus',
+  user: 'test',
+  password: 'test'
+});
+
+app.get('/', async (req, res) => {
+  const result = await client.query('SELECT $1::text as message', ['Hello world!']);
+  res.send(result.rows[0].message);
+});
+
+function onHealthCheck() {
+  return Promise.resolve();
+}
+
+function onSignal() {
+  console.log('server is starting cleanup');
+  return client.end().then(() => console.log('client has disconnected'))
+    .catch(err => console.error('error during disconnection', err.stack));
+}
+
+process.on('exit', (code) => {
+  console.log(`About to exit with code: ${code}`);
+});
+
+async function startServer() {
+  await client.connect();
+  console.log('db connected');
+
+  terminus(http.createServer(app), {
+    logger: console.log,
+    signal: 'SIGINT',
+    healthChecks: {
+      '/healthcheck': onHealthCheck
+    },
+
+    onSignal
+  }).listen(3000);
+}
+
+startServer()
+  .catch(err => console.error('connection error', err.stack));
