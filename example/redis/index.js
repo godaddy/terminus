79224051762f3751e7452e127b4be4ad3a275f4a
Alex Vernacchia <vernak2539@users.noreply.github.com>
2018-01-17 17:26:26 +0000

    docs(examples): add redis

---
+++ example/redis/index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const http = require('http');
+const Redis = require('ioredis');
+const terminus = require('../../lib/terminus');
+
+const app = express();
+let redisClient = {};
+
+function onHealthCheck() {
+  return redisClient.status === 'ready' ? Promise.resolve() : Promise.reject();
+}
+
+function onSignal() {
+  console.log('server is starting cleanup');
+  return redisClient
+    .quit()
+    .then(() => console.log('redis disconnected'))
+    .catch(err => console.error('error during disconnection', err.stack));
+}
+
+async function startServer() {
+  redisClient = new Redis({
+    port: 6379, // Redis port
+    host: '127.0.0.1' // Redis host
+  });
+
+  // OR
+  // redisClient = new Redis.Cluster(/* options */);
+
+  redisClient.on('connect', () => console.log('redis connected'));
+
+  terminus(http.createServer(app), {
+    logger: console.log,
+    signal: 'SIGINT',
+    healthChecks: {
+      '/healthcheck': onHealthCheck
+    },
+    onSignal
+  }).listen(3000);
+}
+
+startServer().catch(err => console.error('connection error', err.stack));
