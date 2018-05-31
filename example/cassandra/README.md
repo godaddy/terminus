d04d473a7d6d035583480789a2d824bbec7f4ede
Łukasz Szewczak <lukaszewczak@gmail.com>
2018-05-31 17:21:21 +0200

    docs(examples): add cassandra

---
+++ example/cassandra/README.md
@@ -0,0 +1,3 @@
+## Run Cassandra using Docker
+`docker build -t terminus-cassandra .`
+`docker run --name terminus-cassandra -p 9042:9042 -d terminus-cassandra`
---
+++ example/cassandra/index.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const http = require('http')
+const cassandra = require('cassandra-driver')
+const terminus = require('../../lib/terminus')
+const app = express()
+const client = new cassandra.Client({contactPoints: ['127.0.0.1']})
+
+function onHealthCheck () {
+  return client.execute('SELECT now() FROM system.local')
+}
+
+async function onSignal () {
+  console.log('server is starting cleanup')
+  try {
+    await client.shutdown()
+    console.log('db has disconnected')
+  } catch (err) {
+    console.error('error during disconnection', err.stack)
+    throw err
+  }
+}
+
+client.connect()
+  .then(() => {
+    console.log('db connected')
+
+    terminus(http.createServer(app), {
+      logger: console.log,
+      signal: 'SIGINT',
+      healthChecks: {
+        '/healthcheck': onHealthCheck
+      },
+
+      onSignal
+    }).listen(3000)
+  })
+  .catch(err => {
+    console.error('connection error', err.stack)
+    client.shutdown()
+  })
