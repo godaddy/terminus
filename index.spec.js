2dfd52d01135c5ea1d458ee624784840bcd49d1c
Gergely Nemeth <gnemeth@uber.com>
2018-09-27 09:21:38 -0700

    test(standalone): use index.js as entry point

--- index.spec.js
+++ index.spec.js
@@ -4,10 +4,10 @@ const { createTerminus, HealthCheckError } = require('./')
 
 describe('Terminus API', () => {
   it('exposes HealthCheckError', () => {
-    expect(HealthCheckError).to.be.a('function');
+    expect(HealthCheckError).to.be.a('function')
   })
 
   it('exposes createTerminus', () => {
-    expect(createTerminus).to.be.a('function');
+    expect(createTerminus).to.be.a('function')
   })
 })
