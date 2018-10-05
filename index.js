e80f7d12011b0355fd7b84dbeafbe6df6b7cfee1
Livio Brunner <livio.brunner.lb1@gmail.com>
2018-10-05 21:25:49 +0200

    Add getHealthStatus method to Terminus class

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 'use strict'
 
 const { createTerminus } = require('./lib/terminus')
-const { HealthCheckError } = require('./lib/terminus-error')
+const { HealthCheckError } = require('./lib/errors/health-check-error')
+const { TerminusError } = require('./lib/errors/terminus-error')
+const { HealthCheckNotFoundError } = require('./lib/errors/health-check-not-found-error')
 
 module.exports = {
   createTerminus,
-  HealthCheckError
+  HealthCheckError,
+  TerminusError,
+  HealthCheckNotFoundError
 }
