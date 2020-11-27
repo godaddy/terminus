d8a5f75773f0161e8ca235a2e0b3c9c5b3e733d3
Srikrishnan Suresh <Srikrishnan.S.Suresh@oracle.com>
2020-11-27 07:45:51 -0800

    Add example for Oracle Database

---
+++ example/oracle/index.js
@@ -0,0 +1,57 @@
+const express = require('express')
+const http = require('http')
+var oracledb = require('oracledb');
+const terminus = require('../../lib/terminus')
+const app = express()
+
+var config = {
+  user: process.env.NODE_ORACLEDB_USER || "scott",
+  password: process.env.NODE_ORACLEDB_PASSWORD || "tiger",
+  connectString: process.env.NODE_ORACLEDB_CONNECTIONSTRING || "localhost:1521/XE",
+  poolMin: 1,
+  poolMax: 10,
+  poolIncrement: 1
+}
+
+async function dbConnect() {
+  await oracledb.createPool(config);
+  var conn = await oracledb.getConnection();
+}
+
+app.get('/dual', async (req, res) => {
+  var conn = await oracledb.getConnection();
+  const result = await conn
+    .execute(`select JSON_OBJECT('id' value sys_guid()) from dual`)
+  res.json(result.rows[0]);
+})
+
+async function onHealthCheck() {
+  var conn = await oracledb.getConnection();
+  console.log('pinging oracle db')
+  return conn.ping();
+}
+
+function onSignal() {
+  console.log('server is starting cleanup')
+  oracledb.getPool().close().then(() => console.log('Oracle database connection closed'))
+    .catch(err => console.error('error during disconnection', err.stack))
+}
+
+async function startServer() {
+  // get oracle connection
+  await dbConnect();
+  console.log('Oracle database connection established')
+
+  terminus.createTerminus(http.createServer(app), {
+    logger: console.log,
+    signal: 'SIGINT',
+    healthChecks: {
+      '/healthcheck': onHealthCheck
+    },
+
+    onSignal
+  }).listen(3000)
+}
+
+startServer()
+  .catch(err => console.error('connection error', err.stack))
