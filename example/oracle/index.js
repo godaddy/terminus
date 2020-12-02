7e844394cbfc2b58522705033df5ac30481f0808
Rick Markins <rmarkins@gmail.com>
2020-12-02 15:01:38 -0700

    feat(promisify): use native promisify and remove es6-promisify (#165)
    * (feat) use native promisify and remove es6-promisify
    
    * (revert) undo promise array
    
    * fix: reverted and removed package via npm uninstall

--- example/oracle/index.js
+++ example/oracle/index.js
@@ -1,45 +1,45 @@
 const express = require('express')
 const http = require('http')
-var oracledb = require('oracledb');
+var oracledb = require('oracledb')
 const terminus = require('../../lib/terminus')
 const app = express()
 
 var config = {
-  user: process.env.NODE_ORACLEDB_USER || "scott",
-  password: process.env.NODE_ORACLEDB_PASSWORD || "tiger",
-  connectString: process.env.NODE_ORACLEDB_CONNECTIONSTRING || "localhost:1521/XE",
+  user: process.env.NODE_ORACLEDB_USER || 'scott',
+  password: process.env.NODE_ORACLEDB_PASSWORD || 'tiger',
+  connectString: process.env.NODE_ORACLEDB_CONNECTIONSTRING || 'localhost:1521/XE',
   poolMin: 1,
   poolMax: 10,
   poolIncrement: 1
 }
 
-async function dbConnect() {
-  await oracledb.createPool(config);
-  var conn = await oracledb.getConnection();
+async function dbConnect () {
+  await oracledb.createPool(config)
+  var conn = await oracledb.getConnection()
 }
 
 app.get('/dual', async (req, res) => {
-  var conn = await oracledb.getConnection();
+  var conn = await oracledb.getConnection()
   const result = await conn
-    .execute(`select JSON_OBJECT('id' value sys_guid()) from dual`)
-  res.json(result.rows[0]);
+    .execute('select JSON_OBJECT(\'id\' value sys_guid()) from dual')
+  res.json(result.rows[0])
 })
 
-async function onHealthCheck() {
-  var conn = await oracledb.getConnection();
+async function onHealthCheck () {
+  var conn = await oracledb.getConnection()
   console.log('pinging oracle db')
-  return conn.ping();
+  return conn.ping()
 }
 
-function onSignal() {
+function onSignal () {
   console.log('server is starting cleanup')
   oracledb.getPool().close().then(() => console.log('Oracle database connection closed'))
     .catch(err => console.error('error during disconnection', err.stack))
 }
 
-async function startServer() {
+async function startServer () {
   // get oracle connection
-  await dbConnect();
+  await dbConnect()
   console.log('Oracle database connection established')
 
   terminus.createTerminus(http.createServer(app), {
